3cf7f98b3c4899a2120572c769d0f7d17f3ea3d5
Angel Rafael Rodriguez Hernandez <angel01rdz@outlook.com>
2021-11-20 15:36:36 -0600

    Final commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import logo from '../images/shared/logo.svg'
 import DeskNav from './navbar/DeskNav'
-import MobileNav from './navbar/MobileNav'
 
 const Header = () => {
     return ( 
         <header className="header">
             <img className="header__logo" src={logo} alt="Space travel agency logo" />
-            {window.innerWidth < 600 ? <MobileNav /> : <DeskNav />}
+            <DeskNav />
         </header>
     );
 }
